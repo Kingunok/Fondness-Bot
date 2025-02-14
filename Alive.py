28bd30bbb2854764c75325f14e3c6d6f7d3ed381
Kingunok <114995588+Kingunok@users.noreply.github.com>
2025-02-15 00:07:48 +0530

    Update Alive.py

--- Alive.py
+++ Alive.py
@@ -3,14 +3,14 @@ from requests import get as rget
 from os import environ
 from logging import error as logerror
 
-BASE_URL = environ.get('BASE_URL', None)
+BASE_URL = environ.get('BASE_URL', 8080)
 try:
     if len(BASE_URL) == 0:
         raise TypeError
     BASE_URL = BASE_URL.rstrip("/")
 except TypeError:
     BASE_URL = None
-PORT = environ.get('PORT', None)
+PORT = environ.get('PORT', 8080)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
